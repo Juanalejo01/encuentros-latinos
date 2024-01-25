e0a6e29c830621449e389cdd7794f8aefcab49bc
Juan <juanangeloti771@gmail.com>
2024-01-25 15:58:46 +0100

    union de registro y backend

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,74 +1,106 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { registerUserService } from "../services/registerUserServices";
 
 export const RegisterPage = () => {
-  const [, setFormData] = useState({
-    nombre: "",
-    apellidos: "",
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    descripcion: "",
-    avatar: null,
-  });
+  const navigate = useNavigate();
+  const [nombre, setNombre] = useState("");
+  const [apellidos, setApellidos] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [biografia, setBiografia] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [error, setError] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value, type } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: type === "file" ? e.target.files[0] : value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
+  const handleForm = async (e) => {
     e.preventDefault();
-    // Lógica para manejar el envío del formulario
-  };
 
+    if (password !== confirmPassword) {
+      setError("Las contraseñas no coinciden");
+      return;
+    }
+
+    try {
+      await registerUserService(
+        nombre,
+        apellidos,
+        email,
+        password,
+        biografia,
+        avatar
+      );
+
+      //ir a login
+
+      navigate("/login");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   return (
     <section className="layout__registro">
       <h1 className="registro__title">Registro de Usuario</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Nombre:</label>
-        <input type="text" name="nombre" required onChange={handleChange} />
-
-        <label>Apellidos:</label>
-        <input type="text" name="apellidos" required onChange={handleChange} />
-
-        <label>Username:</label>
-        <input type="text" name="username" required onChange={handleChange} />
-
-        <label>Email:</label>
-        <input type="email" name="email" required onChange={handleChange} />
-
-        <label>Contraseña:</label>
+      <form onSubmit={handleForm}>
+        <label htmlFor="nombre">Nombre:</label>
         <input
-          type="password"
-          name="password"
+          type="text"
+          name="nombre"
           required
-          onChange={handleChange}
+          onChange={(e) => setNombre(e.target.value)}
         />
 
-        <label>Confirmar Contraseña:</label>
+        <label htmlFor="apellidos">Apellidos:</label>
+        <input
+          type="text"
+          name="apellidos"
+          required
+          onChange={(e) => setApellidos(e.target.value)}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <label htmlFor="confirmPassword">Confirmar Contraseña:</label>
         <input
           type="password"
           name="confirmPassword"
           required
-          onChange={handleChange}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
 
-        <label>Descripción:</label>
+        <label htmlFor="biografia">Biografia:</label>
         <textarea
-          name="descripcion"
+          name="biografia"
           rows="4"
           cols="50"
-          onChange={handleChange}
+          onChange={(e) => setBiografia(e.target.value)}
         />
 
-        <label>Subir Avatar:</label>
-        <input type="file" name="avatar" onChange={handleChange} />
+        <label htmlFor="avatar">Subir Avatar:</label>
+        <input
+          type="file"
+          name="avatar"
+          onChange={(e) => setAvatar(e.target.value)}
+        />
 
         <button type="submit">Registrarse</button>
+        {error ? <p> {error} </p> : null}
       </form>
     </section>
   );
