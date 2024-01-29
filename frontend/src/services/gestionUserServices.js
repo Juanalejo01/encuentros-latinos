47fa5f3860dced82204a74aac1606893f44026b6
Juan <juanangeloti771@gmail.com>
2024-01-29 20:07:19 +0100

    contexto, token y loginPage

--- frontend/src/services/gestionUserServices.js
+++ frontend/src/services/gestionUserServices.js
@@ -18,3 +18,20 @@ export const registerUserService = async ({ data }) => {
     throw new Error(`${error.message}`);
   }
 };
+
+export const loginUserService = async ({ email, password }) => {
+  const response = await fetch(`${import.meta.env.VITE_APP_BACKEND}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(json.message);
+  }
+  return json;
+};
