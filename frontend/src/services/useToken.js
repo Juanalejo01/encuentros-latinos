fd0105006ba40160184755bf0184a8d589c6d0cb
jose carmona zuñiga <carmonajcz@gmail.com>
2024-02-01 16:56:50 +0100

    Se elimina el servicio de useToken

--- frontend/src/services/useToken.js
+++
@@ -1,6 +0,0 @@
-export const useToken = () => {
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzA2NDM3MTcwLCJleHAiOjE3MDY1MjM1NzB9.9MCIkW-HdQnHrWn7dIwMZzpiYAB9GfOjvmK3K4haetA";
-
-  return { token };
-};
