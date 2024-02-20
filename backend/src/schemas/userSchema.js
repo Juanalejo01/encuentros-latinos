2f61f833ae33945cf5450131ec26485405efb880
Jose <carmonajcz@gmail.com>
2024-02-20 15:25:53 +0100

    Se arregla algunos detalles de CSS y de la regla del email que sea en minúscula

--- backend/src/schemas/userSchema.js
+++ backend/src/schemas/userSchema.js
@@ -5,19 +5,20 @@ const customMessages = {
   "string.min": "El campo {#label} debe tener al menos {#limit} caracteres",
   "string.max": "El campo {#label} debe tener como máximo {#limit} caracteres",
   "string.email": "El campo {#label} debe ser una dirección de correo electrónico válida",
+  "string.lowercase": "El campo {#label} debe estar en minúscula",
 };
 
 export const registroSchema = Joi.object({
   nombre: Joi.string().trim().min(3).max(30).required().messages(customMessages),
   apellidos: Joi.string().trim().min(3).max(50).required().messages(customMessages),
-  email: Joi.string().trim().email().max(100).required().messages(customMessages),
+  email: Joi.string().trim().email().max(100).required().lowercase().messages(customMessages),
   password: Joi.string().trim().min(8).required().messages(customMessages),
   biografia: Joi.string().max(200).allow("").messages(customMessages),
   avatar: Joi.string().trim().min(100).allow("").messages(customMessages),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().trim().email().max(100).required().messages(customMessages),
+  email: Joi.string().trim().email().max(100).required().lowercase().messages(customMessages),
   password: Joi.string().trim().min(8).required().messages(customMessages),
 });
 
@@ -29,7 +30,7 @@ export const updateSchema = Joi.object({
 });
 
 export const emailSchema = Joi.object({
-  email: Joi.string().trim().email().max(100).required().messages(customMessages),
+  email: Joi.string().trim().email().max(100).required().lowercase().messages(customMessages),
   password: Joi.string().trim().min(8).required().messages(customMessages),
 });
 
